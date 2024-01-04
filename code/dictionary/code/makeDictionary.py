c48319715d75139673fbf88dffd4d9f58162924d
FernandoCF7 <cuentadef1@hotmail.com>
2024-01-04 12:55:31 -0600

    add the makeDictionary stage

---
+++ code/dictionary/code/makeDictionary.py
@@ -0,0 +1,37 @@
+"""
+Created on Mon Jan 20 12:50:32 2020
+
+@author: fernando
+"""
+
+#-----------------------------------------------------------------------------#
+import os
+import pandas as pd
+import h5py
+from sklearn.cluster import MiniBatchKMeans
+import pickle
+#-----------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+#Make the dictionary
+with h5py.File(os.path.join("..", "..", "variables", "HOG", "HOGallImgs.hdf5"), "r") as f:
+    list_ImgsNames = list(f.keys())
+    
+    kmeans = MiniBatchKMeans(
+        n_clusters=300,
+        init='k-means++',
+        random_state=0,
+        compute_labels=False
+    ).fit( f[list_ImgsNames[0]][()] )
+    
+    for image in list_ImgsNames[1:]:
+        kmeans.partial_fit( f[image][()] )
+#-----------------------------------------------------------------------------#
+
+#-----------------------------------------------------------------------------#
+#Store the dictionary
+pickle.dump(
+    kmeans,
+    open(os.path.join("..", "..", "variables", "dictionary", "dict_300words.pkl"), "wb")
+)
+#-----------------------------------------------------------------------------#
